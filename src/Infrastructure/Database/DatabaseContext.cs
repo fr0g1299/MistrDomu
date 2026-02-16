c4730dbf08da5886b213fce48d8cce77c4942734
Jiri Drapal <41378892+JustJirka@users.noreply.github.com>
2026-02-16 08:15:03 +0100

    changed to react template

--- src/Infrastructure/Database/DatabaseContext.cs
+++
@@ -1,9 +0,0 @@
-using Application.Infrastructure.Database.Models;
-using Microsoft.EntityFrameworkCore;
-
-namespace Application.Infrastructure.Database;
-
-public class DatabaseContext(DbContextOptions options) : DbContext(options)
-{
-    public DbSet<UserDo> Users => Set<UserDo>();
-}
