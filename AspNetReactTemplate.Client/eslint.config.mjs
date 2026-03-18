fc26dd491dc4d8104f0177d38be7b44cc7d66e37
Martin Andrýsek <m_andrysek@utb.cz>
2026-03-18 22:55:45 +0100

    chore: update packages and add eslint config

---
+++ AspNetReactTemplate.Client/eslint.config.mjs
@@ -0,0 +1,39 @@
+import { defineConfig } from "eslint/config";
+import tsParser from "@typescript-eslint/parser";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+
+export default defineConfig([
+  {
+    ignores: ["dist"],
+  },
+  {
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+      semi: "error",
+      "prefer-const": "error",
+    },
+  },
+]);
