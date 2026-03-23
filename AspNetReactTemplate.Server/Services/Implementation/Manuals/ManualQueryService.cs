db5c69546ebf0cd9e77d4645f70bed95731c8791
Dominik Vachl <vachlino@seznam.cz>
2026-03-23 11:25:12 +0100

    added unnaccent extension to db

--- AspNetReactTemplate.Server/Services/Implementation/Manuals/ManualQueryService.cs
+++ AspNetReactTemplate.Server/Services/Implementation/Manuals/ManualQueryService.cs
@@ -41,7 +41,9 @@ public class ManualQueryService : IManualQueryService
         var pattern = $"%{keyword.Trim()}%";
 
         var manuals = await BuildManualQuery(includeSteps)
-            .Where(m => EF.Functions.ILike(m.Title, pattern) || EF.Functions.ILike(m.Description, pattern) || m.Tags.Any(tag => EF.Functions.ILike(tag, pattern)))
+            .Where(m =>EF.Functions.ILike(EF.Functions.Unaccent(m.Title), EF.Functions.Unaccent(pattern)) ||
+                EF.Functions.ILike(EF.Functions.Unaccent(m.Description), EF.Functions.Unaccent(pattern)) || 
+                m.Tags.Any(tag => EF.Functions.ILike(EF.Functions.Unaccent(tag), EF.Functions.Unaccent(pattern))))
             .ToListAsync();
 
         return manuals.Select(manual => MapToReadDto(manual, includeSteps));
