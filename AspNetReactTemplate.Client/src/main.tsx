50d20625f093d1483ea937237c10acaa22480444
Martin Andrýsek <m_andrysek@utb.cz>
2026-04-06 18:30:48 +0200

    chore: clean up theme handling and move unused Theme-related files

--- AspNetReactTemplate.Client/src/main.tsx
+++ AspNetReactTemplate.Client/src/main.tsx
@@ -3,14 +3,11 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
-import { ThemeProvider } from "./components/providers/ThemeProvider.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
+      <App />
     </BrowserRouter>
   </React.StrictMode>,
 );
